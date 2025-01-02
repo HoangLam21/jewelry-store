b9b8e830084b1730b013962991f5d16d54851488
HoangLam21 <hoanglamdeveloper@gmail.com>
2025-01-02 22:31:34 +0700

    Add more properties to Provider

--- database/provider.model.ts
+++ database/provider.model.ts
@@ -5,12 +5,18 @@ export interface IProductProvider extends Document, IAudit {
   name: string;
   address: string;
   contact: string;
+  representativeName:string;
+  city:string;
+  country:string;
 }
 
 const ProductProviderSchema = new Schema<IProductProvider>({
   name: { type: String, required: true },
   address: { type: String, required: true },
   contact: { type: String, required: true },
+  representativeName:{type:String, required:true},
+  city:{type:String, required:true},
+  country:{type:String, required:true}
 });
 
 ProductProviderSchema.add(AuditSchema);
