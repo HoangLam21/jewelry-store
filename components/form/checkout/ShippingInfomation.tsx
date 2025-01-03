163768ed72c1870a6d63ba7e880bd15bc0e931db
22520592 <22520592@gm.uit.edu.vn>
2025-01-03 22:24:14 +0700

    create order

--- components/form/checkout/ShippingInfomation.tsx
+++ components/form/checkout/ShippingInfomation.tsx
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
-const ShippingInfomation = ({ city, setCity }: any) => {
+const ShippingInfomation = ({
+  city,
+  setCity,
+  setAddress,
+  setPhoneNumber,
+  setNote,
+}: any) => {
+  const [district, setDistrict] = useState("");
+  const [street, setStreet] = useState("");
+
+  const combineAddress = () => {
+    const fullAddress = `${street}, ${district}, ${city}`;
+    setAddress(fullAddress);
+  };
+
   return (
     <div>
       <label className="font-light text-[16px]">
@@ -9,16 +23,16 @@ const ShippingInfomation = ({ city, setCity }: any) => {
       <input
         type="text"
         placeholder="Name"
-        className="w-full p-3 border  bg-transparent dark:bg-dark-400"
+        className="w-full p-3 border bg-transparent dark:bg-dark-400"
       />
-      <label className="font-light mt-2 text-[16px]">
+      {/* <label className="font-light mt-2 text-[16px]">
         Country / Region<span className="text-primary-100">*</span>
       </label>
       <input
         type="text"
         placeholder="Country"
-        className="w-full p-3 border  bg-transparent dark:bg-dark-400"
-      />
+        className="w-full p-3 border bg-transparent dark:bg-dark-400"
+      /> */}
       <label className="font-light mt-2 text-[16px]">
         City / Province<span className="text-primary-100">*</span>
       </label>
@@ -26,8 +40,11 @@ const ShippingInfomation = ({ city, setCity }: any) => {
         type="text"
         placeholder="City"
         value={city}
-        onChange={(e) => setCity(e.target.value)}
-        className="w-full p-3 border  bg-transparent dark:bg-dark-400"
+        onChange={(e) => {
+          setCity(e.target.value);
+          combineAddress(); // Gọi hàm mỗi khi giá trị thay đổi
+        }}
+        className="w-full p-3 border bg-transparent dark:bg-dark-400"
       />
       <label className="font-light mt-2 text-[16px]">
         District<span className="text-primary-100">*</span>
@@ -35,7 +52,12 @@ const ShippingInfomation = ({ city, setCity }: any) => {
       <input
         type="text"
         placeholder="District"
-        className="w-full p-3 border  bg-transparent dark:bg-dark-400"
+        value={district}
+        onChange={(e) => {
+          setDistrict(e.target.value);
+          combineAddress();
+        }}
+        className="w-full p-3 border bg-transparent dark:bg-dark-400"
       />
       <label className="font-light mt-2 text-[16px]">
         Street address<span className="text-primary-100">*</span>
@@ -43,7 +65,12 @@ const ShippingInfomation = ({ city, setCity }: any) => {
       <input
         type="text"
         placeholder="Street Address"
-        className="w-full p-3 border  bg-transparent dark:bg-dark-400"
+        value={street}
+        onChange={(e) => {
+          setStreet(e.target.value);
+          combineAddress();
+        }}
+        className="w-full p-3 border bg-transparent dark:bg-dark-400"
       />
       <label className="font-light mt-2 text-[16px]">
         Phone<span className="text-primary-100">*</span>
@@ -51,22 +78,24 @@ const ShippingInfomation = ({ city, setCity }: any) => {
       <input
         type="text"
         placeholder="Phone"
-        className="w-full p-3 border  bg-transparent dark:bg-dark-400"
+        onChange={(e) => setPhoneNumber(e.target.value)}
+        className="w-full p-3 border bg-transparent dark:bg-dark-400"
       />
-      <label className="font-light mt-2 text-[16px]">
+      {/* <label className="font-light mt-2 text-[16px]">
         Email<span className="text-primary-100">*</span>
       </label>
       <input
         type="email"
         placeholder="Email"
-        className="w-full p-3 border  bg-transparent dark:bg-dark-400"
-      />
+        className="w-full p-3 border bg-transparent dark:bg-dark-400"
+      /> */}
       <label className="font-light mt-2 text-[16px]">
         Note<span className="text-primary-100"></span>
       </label>
       <textarea
         placeholder="Note"
         rows={4}
+        onChange={(e) => setNote(e.target.value)}
         className="w-full p-3 border mb-2 bg-transparent dark:bg-dark-400"
       ></textarea>
     </div>
