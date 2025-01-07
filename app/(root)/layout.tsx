85131619ec7b23ac49d8b4369c4c77ff81f7e984
22520592 <22520592@gm.uit.edu.vn>
2025-01-07 23:43:03 +0700

    FE Customer collection

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -7,7 +7,7 @@ import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   const pathname = usePathname();
-  const isAdmin = pathname.startsWith("/admin");
+  const isAdmin = pathname?.startsWith("/admin");
 
   const links = isAdmin ? true : false;
   return (
