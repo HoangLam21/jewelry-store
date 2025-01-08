69704dc5eb5ae79d9d27d982fa08ef743e6cb918
22520592 <22520592@gm.uit.edu.vn>
2025-01-08 13:25:02 +0700

    fix

--- database/cart.model.ts
+++ database/cart.model.ts
@@ -1,19 +1,6 @@
 import { Schema, model, models, Document } from "mongoose";
 import { ObjectId } from "mongodb";
 import { AuditSchema, IAudit } from "./audit.model";
-
-// export interface ICart extends Document, IAudit {
-//     user: Schema.Types.ObjectId;
-//     totalCost: number;
-//     details: Map<ObjectId, number>;
-// }
-
-// const CartSchema = new Schema<ICart>({
-//     user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-//     totalCost: { type: Number, required: true },
-//     details: { type: Map, of: Number, required: true },
-// });
-
 interface ICartDetail {
   quantity: number;
   selectedMaterial: string;
